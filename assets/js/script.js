f744dc9f3abc2417f90bb438b563bf3644c537fc
clementsem <clementctes@gmail.com>
2019-01-29 09:46:21 +0100

    js3exo4

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+function pwMatch() {
+    var password = document.getElementById("password").value;
+    var confirmPassword = document.getElementById("confirmPassword").value;
+
+    if (password != confirmPassword){
+      document.getElementById("password").style.borderColor = "red";
+      document.getElementById("confirmPassword").style.borderColor = "red";
+    } else {
+      document.getElementById("password").style.borderColor = "green";
+      document.getElementById("confirmPassword").style.borderColor = "green";
+    }
+  }
